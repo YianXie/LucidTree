d5c6b60139105ab7136b30fb4babfab410e636e0
YianXie <yian.xie@outlook.com>
2026-01-05 12:20:40 +0800

    refactor: renamed .sgf data and removed outdated code in main.py

--- src/mini_katago/main.py
+++ src/mini_katago/main.py
@@ -2,19 +2,12 @@
 A file for testing
 """
 
-from sgfmill import sgf
-
 from mini_katago.board import Board
 from mini_katago.constants import BLACK_COLOR, WHITE_COLOR
 from mini_katago.player import Player
 
-with open("src/mini_katago/data/test_territories.sgf", "rb") as f:
-    game = sgf.Sgf_game.from_bytes(f.read())
-root_node = game.get_root()
-black_player = Player(root_node.get("PB"), BLACK_COLOR)
-white_player = Player(root_node.get("PW"), WHITE_COLOR)
-# moves = [node.get_move() for node in game.get_main_sequence()]
-
+black_player = Player("Black Player", BLACK_COLOR)
+white_player = Player("White Player", WHITE_COLOR)
 board = Board(9, black_player, white_player)
 color = BLACK_COLOR
 
@@ -31,12 +24,3 @@ try:
         board.print_ascii_board()
 except Exception as e:
     print("Error:", e)
-
-# for move in moves:
-#     if None in move:
-#         continue
-#     color, row, col = move[0], move[1][0], move[1][1]
-#     board.place_move((row, col), -1 if color == "b" else 1)
-
-# print(board.calculate_score())
-# board.show_board()
