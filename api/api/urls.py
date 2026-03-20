076b1f07d10bb405af167e333eada34b4c15cbb0
YianXie <yian.xie@outlook.com>
2026-03-20 10:51:22 +0800

    feat: updated static files related settings

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.admin.sites import settings
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("game_api.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
